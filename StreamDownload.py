de5f9d4656d78adc2a2def81f23d9367f540b905
wangminqi <wangminqi@aliyun.com>
2019-02-26 14:40:34 +0800

    2019/2/25 Overall Update

--- StreamDownload.py
+++ StreamDownload.py
@@ -1,13 +1,13 @@
 import streamlink
-streams = streamlink.streams("https://www.douyu.com/70231")
-stream = streams["source"]
-fd = stream.open()
-data = fd.read(1024)
-fd.close()
+#streams = streamlink.streams("https://www.douyu.com/3284257")
+streams = streamlink.streams("https://www.huya.com/964376")
+
 
 print(streams)
 
-# session = streamlink.Streamlink()
-# streams = session.streams("https://www.douyu.com/70231")
-# session.set_option("rtmp-rtmpdump", "/path/to/rtmpdump")
 #
+
+
+
+
+
